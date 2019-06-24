07029c5b5193cdd794ad3842a92893c07a5ce550
Logan Emerson <logemerson@gmail.com>
2019-06-24 18:13:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
 
-const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+const shortGreeting = 'Welcome, ' + currentUser.slice(1) + '!';
