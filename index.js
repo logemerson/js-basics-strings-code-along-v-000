9f7ec450374f33f23c0f93238ed90ee6fa9d109b
Logan Emerson <logemerson@gmail.com>
2019-06-24 18:11:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const currentUser = 'Cincinnatus';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
